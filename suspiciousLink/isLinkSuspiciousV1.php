5f111a7471c30e6437635f6fe66c1447e14e8e91
Natalie Nyshta <natalya.nyshta@paxful.com>
2021-02-28 19:06:42 +0200

    is link suspicious article

---
+++ suspiciousLink/isLinkSuspiciousV1.php
@@ -0,0 +1,16 @@
+<?php
+
+function isLinkNotSuspicious(string $link)
+{
+    if (linkContainsReservedTerms($link) ||
+        isDomainNew($link) ||
+        isLinkShortened($link)
+    ) {
+        return false;
+    } else {
+        log('Link is ok');
+        return true;
+    }
+}
+
+if (!isLinkNotSuspicious($someLink)) {...}
